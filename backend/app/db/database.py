d66df2f83d938c111597d8c14ce53feda1ee2b29
Rudra Pratap Singh <rudra888156@gmail.com>
2026-03-19 13:23:43 +0530

    feat(database): implement async database engine and session management

---
+++ backend/app/db/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from typing import AsyncGenerator
+from app.core.config import settings
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_ = AsyncSession,
+    expire_on_commit=False
+)
