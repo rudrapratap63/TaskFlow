3df3243149bc7488a0d2bf2753c7d070b2c145d2
Rudra Pratap Singh <rudra888156@gmail.com>
2026-03-20 12:52:35 +0530

    feat(auth): implement user authentication with JWT and add current user retrieval endpoint

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,5 +1,16 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy import or_, select
+
 from app.db.database import AsyncSessionLocal, AsyncSession
 from typing import AsyncGenerator
+
+from app.core.config import settings
+from app.crud.user_crud import check_user_exist
+from app.db import models
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 async def get_db() -> AsyncGenerator[AsyncSession, None] :
     async with AsyncSessionLocal() as session:
@@ -7,3 +18,35 @@ async def get_db() -> AsyncGenerator[AsyncSession, None] :
             yield session
         finally:
             await session.close()
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        401,
+        detail="Could not validate credentials"
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        print(payload)
+        email = payload.get("email")
+        username = payload.get("username")
+
+        if email is None or username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    stmt = select(models.User).where(
+            or_(
+                models.User.email == email,
+                models.User.username == username
+            )
+        )
+
+    result = await db.execute(stmt)
+
+    user = result.scalars().first()
+
+    if user is None:
+        raise credentials_exception
+    
+    return user
--- backend/app/api/routes/user_routes.py
+++ backend/app/api/routes/user_routes.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.deps import get_db
+from app.api.deps import get_current_user, get_db
 from app.schemas.user_schema import UserCreate, LoginRequest, UserResponse, Token
 from app.crud.user_crud import check_user_exist, create_user
 from app.core.security import create_access_token
 from app.utils.hash import verify_password
+from app.db.models import User
 
 router = APIRouter(
     prefix="/users",
@@ -38,3 +39,7 @@ async def signin(user: LoginRequest, db: AsyncSession = Depends(get_db)):
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+@router.get("/me", response_model=UserResponse)
+async def check_me(current_user: User = Depends(get_current_user)):
+    return current_user
