9724d9488680310de3c85c03d80bb2a685dbefbe
Rudra Pratap Singh <rudra888156@gmail.com>
2026-03-20 09:35:47 +0530

    feat(crud): hash user password before storing in the database

--- backend/app/crud/user_crud.py
+++ backend/app/crud/user_crud.py
@@ -2,15 +2,16 @@ from sqlalchemy.orm import Session
 
 from app.schemas.user_schema import UserCreate
 from backend.app.db import models
+from backend.app.utils.hash import hash_password
 
 
 def create_user(db: Session, user: UserCreate):
-    
+    hash_pwd = hash_password(user.password)
     new_user = models.User(
         name = user.name,
         email = user.email,
         username = user.username,
-        password = user.password
+        password = hash_pwd
     )
 
     db.add(new_user)
